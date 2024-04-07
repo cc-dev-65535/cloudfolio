d0908f39698a55d2bf86d36fb857f979b3a1d563
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2024-04-07 14:10:54 -0700

    FInal version for local

--- www.js
+++ www.js
@@ -1,12 +1,3 @@
-// import app from "./app.js";
-// import http from "http";
-
-// const port = process.env.PORT || "3200";
-// app.set("port", port);
-
-// const server = http.createServer(app);
-// server.listen(port);
-
 import app from "./app.js";
 import http from "http";
 
