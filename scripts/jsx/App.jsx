41ff4ed181549c91b0194495953578486e564886
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2024-04-07 13:51:42 -0700

    Organizing album and app

--- scripts/jsx/App.jsx
+++ scripts/jsx/App.jsx
@@ -1,36 +1,9 @@
-// import { BrowserRouter, RouterProvider, Routes, Route } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import Album from "./Album.jsx";
-// import Home from "./Home.jsx";
-// import Sidebar from "./Sidebar.jsx";
-
-// const queryClient = new QueryClient();
-
-// function App() {
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <BrowserRouter>
-//                 <Routes>
-//                     <Route path="/" element={<Sidebar />}>
-//                         <Route path="/" element={<Home />} />
-//                         <Route path="album/*" element={<Album />} />
-//                     </Route>
-//                 </Routes>
-//             </BrowserRouter>
-//         </QueryClientProvider>
-//     );
-// }
-
-// export { queryClient };
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Album from "./Album.jsx";
 import Home from "./Home.jsx";
 import Sidebar from "./Sidebar.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ThemeProvider } from '@mui/material/styles/index.js';
 import theme from "../theme/theme";
 import "./global.css";
 import { ThemeProvider, AppBar, Toolbar, Button, Box } from "@mui/material";
